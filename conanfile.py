33443fe80fc32a7e1ccdde691c08e1108b8ebd08
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:32:31 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -2,17 +2,17 @@ from conans import ConanFile, tools, os
 
 class BoostHanaConan(ConanFile):
     name = "Boost.Hana"
-    version = "1.64.0"
+    version = "1.65.1"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-hana"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["hana"]
-    requires =  "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Core/1.64.0@bincrafters/testing", \
-                      "Boost.Fusion/1.64.0@bincrafters/testing", \
-                      "Boost.Mpl/1.64.0@bincrafters/testing", \
-                      "Boost.Tuple/1.64.0@bincrafters/testing"
+    requires =  "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Core/1.65.1@bincrafters/testing", \
+                      "Boost.Fusion/1.65.1@bincrafters/testing", \
+                      "Boost.Mpl/1.65.1@bincrafters/testing", \
+                      "Boost.Tuple/1.65.1@bincrafters/testing"
 
                       #config0 core2 fusion5 mpl5 tuple4
 
